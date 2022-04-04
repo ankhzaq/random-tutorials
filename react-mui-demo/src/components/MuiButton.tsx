6d7f10a2de392b9a84f6677dd472660544b4c6cc
Zaquiel Rodriguez Arce <zaquielverse@gmail.com>
2022-04-04 05:52:21 +0200

    feature - stack and toggle button added

--- react-mui-demo/src/components/MuiButton.tsx
+++ react-mui-demo/src/components/MuiButton.tsx
@@ -1,7 +1,13 @@
-import { Button, IconButton, Stack } from '@mui/material';
+import { Button, ButtonGroup, IconButton, Stack, ToggleButton, ToggleButtonGroup } from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
+import { FormatBold, FormatItalic, FormatUnderlined } from '@mui/icons-material';
+import React, { useState } from 'react';
 
 export const MuiButton = () => {
+  const [ formats, setFormats ] = useState<string | null>(null);
+  const handleFormatChanges = (_event: React.MouseEvent<HTMLElement>, updatedFormats: string | null) => {
+    setFormats(updatedFormats);
+  }
   return (
     <Stack spacing={4}>
       <Stack spacing={2} direction='row'>
@@ -28,6 +34,30 @@ export const MuiButton = () => {
         <IconButton aria-label='send' color='success' size='small'>
           <SendIcon />
         </IconButton>
+      </Stack>
+      <Stack direction='row'>
+        <ButtonGroup variant='contained'>
+          <Button variant='contained'>Left</Button>
+          <Button variant='contained'>Center</Button>
+          <Button variant='contained'>Right</Button>
+        </ButtonGroup>
+        <ButtonGroup variant='text'>
+          <Button >Left</Button>
+          <Button >Center</Button>
+          <Button >Right</Button>
+        </ButtonGroup>
+        <ButtonGroup aria-label='alignment button group' variant='text' orientation='vertical' size='small' color='secondary'>
+          <Button onClick={() => alert('left click')}>Left</Button>
+          <Button >Center</Button>
+          <Button >Right</Button>
+        </ButtonGroup>
+      </Stack>
+      <Stack direction='row'>
+        <ToggleButtonGroup aria-label='text formatting' value={formats} onChange={handleFormatChanges} size='small' color='success' orientation='vertical' exclusive>
+          <ToggleButton value='bold'><FormatBold /></ToggleButton>
+          <ToggleButton value='italic'><FormatItalic /></ToggleButton>
+          <ToggleButton value='underlined'><FormatUnderlined /></ToggleButton>
+        </ToggleButtonGroup>
       </Stack>
     </Stack>
   );
