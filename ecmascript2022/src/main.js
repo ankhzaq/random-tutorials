1a432b47e9c0b4bd92ca5adfcf5352dd721ae23a
Zaquiel Rodriguez Arce <zaquielverse@gmail.com>
2022-06-09 19:41:29 +0200

    feature - second feature await outside function

--- ecmascript2022/src/main.js
+++ ecmascript2022/src/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import User from './features/privateProperties';
+// import awaitTop from './features/awaiTop';
 
 function counter() {
   let seconds = 0;
@@ -12,10 +13,20 @@ function counter() {
 
 counter();
 
-// First feature -> '#' symbols makes a variable or method private, so you can't access to those private variables/methods out of the file.
+// FIRST FEATURE -> '#' symbols makes a variable or method private, so you can't access to those private variables/methods out of the file.
 
 const user = new User("zaquielverse@gmail.com");
 
 // console.log(user.#id === 1); // this line throw an error because #id is a private variable
 // console.log(user.#checkEmailIsDefined("zaquielverse@gmail.com")); // this line throw an error because #checkEmailIsDefined is a private method
+
 console.log(user.email); // This line is correct because email is a public variable.
+
+//SECOND FEATURE -> you can use await outside of async function
+
+const response = await fetch("https://reqres.in/api/users?api=1");
+
+const json = await response.json();
+console.log(json);
+
+
